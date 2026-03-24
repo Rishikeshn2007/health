a04142bca0494d258d5c65146e26d66c05a641fa
Rishikeshn2007 <rishikeshn663@gmail.com>
2026-03-24 21:34:59 +0530

    Rishi

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express=require('express');
+require('dotenv').config()
+const port=process.env.PORT;
 
+const app=express();
+
+//Internal packages
+
+app.use('/',(req,res)=>{
+    res.send('Server runnig successfully!')
+})
+
+app.listen(port,(req,res)=>{
+    console.log(`Server running on 127.0.0.1:${port}`);
+});
